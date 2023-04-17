61a950df6309ff36c8c5d93fb2f5097236400c47
lkaminka <lkaminka25@amherst.edu>
2023-04-17 17:02:25 -0400

    Initial commit

---
+++ src/ParallelPrimes.java
@@ -0,0 +1,12 @@
+public class ParallelPrimes {
+
+    // replace this string with your team name
+    public static final String TEAM_NAME = "baseline";
+
+    public static void optimizedPrimes(int[] primes) {
+
+        // replace this with your optimized method
+        Primes.baselinePrimes(primes);
+
+    }
+}
